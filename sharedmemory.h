ebdf96779fa4c8bd70fb850dcbf5dcbfd43f8d3e
ClementPagran <83000752+ClementPagran@users.noreply.github.com>
2021-11-30 17:03:17 +0100

    Update sharedmemory.h

--- sharedmemory.h
+++ sharedmemory.h
@@ -2,8 +2,32 @@
 #define SHAREDMEMORY_H
 #include <stdbool.h>
 
-void * attach_memory_block(char *nomfichier,int taille); //(cree et )attache la memoire partagee, nomfichier doit exister, taille pour 3 floats : 3*sizeof(float)
-bool detach_memory_block(void *block); //detache la memoire partagee
-bool destroy_memory_block(char *nomfichier); //supprime la memoire partagee (a appeler si plus de processus attache a cette memoire)
+//########################################
+//Crée et attache une mémoire partagée a un fichier avec un taille donné
+//Input:
+//  char *nomfichier : path vers un fichier qui va etre la mémoire partagé
+//  int taille : taille de cette mémoire (3*sizeof(float) pour une taille de 3 float)
+//Output:
+//  void * : pointeur vers le mémoire créé
+//########################################
+void * attach_memory_block(char *nomfichier,int taille);
+
+//########################################
+//Detache la mémoire du processus
+//Input:
+//  void *block : memoire a détacher
+//Output:
+//  bool : boolean si operation réussi
+//########################################
+bool detach_memory_block(void *block);
+
+//########################################
+//Supprime la mémoire partagée
+//Input:
+//  char * : nom de fichier utilisé pour créer la mémoire
+//Output:
+//  bool : boolean si operation réussi
+//########################################
+bool destroy_memory_block(char *nomfichier);
 
 #endif
