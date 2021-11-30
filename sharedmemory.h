c0f370d522ea1beafc7921a0216b1a4225527372
ClementPagran <83000752+ClementPagran@users.noreply.github.com>
2021-11-30 16:50:16 +0100

    Update sharedmemory.h

--- sharedmemory.h
+++ sharedmemory.h
@@ -1,10 +1,9 @@
 #ifndef SHAREDMEMORY_H
 #define SHAREDMEMORY_H
-
 #include <stdbool.h>
 
-void * attach_memory_block(char *nomfichier,int taille);
-bool detach_memory_block(void *block);
-bool destroy_memory_block(char *nomfichier);
+void * attach_memory_block(char *nomfichier,int taille); //(cree et )attache la memoire partagee, nomfichier doit exister, taille pour 3 floats : 3*sizeof(float)
+bool detach_memory_block(void *block); //detache la memoire partagee
+bool destroy_memory_block(char *nomfichier); //supprime la memoire partagee (a appeler si plus de processus attache a cette memoire)
 
 #endif
