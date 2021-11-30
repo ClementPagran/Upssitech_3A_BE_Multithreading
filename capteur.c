12f4a2764c2e5bd8d2539fa62ca6ca9b50a51a35
Hakimcherfi <60069195+Hakimcherfi@users.noreply.github.com>
2021-11-30 19:45:06 +0100

    Update capteur.c

--- capteur.c
+++ capteur.c
@@ -5,7 +5,11 @@
 int main()
 {
 	float* mem;
-    mem = (float*)attach_memory_block("Memoire/data_capteur.mem",sizeof(float));
+	if(mem = (float*)attach_memory_block("./Memoire/data_capteur.mem",sizeof(float))== NULL)
+	{
+		printf("erreur : capteur n'a pas acces au bloc \"Memoire/data_capteur.mem\"\n");
+		return -1;
+	}
 	*mem = random()%30; //genere la donnee (temperature aleatoire entre 0 et 29 degres)
 	//detach_memory_block(mem);
     //destroy_memory_block("sharedmem");
